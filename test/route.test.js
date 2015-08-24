408d261b4129a524258d562b3e765f09ec390ffe
DavidCai <376462191@qq.com>
2015-08-24 10:47:06 +0800

    add utils & optimize test

--- test/route.test.js
+++ test/route.test.js
@@ -1,15 +1,27 @@
 'use strict';
-let request = require('co-supertest');
 let app = require('../lib/index');
+let request = require('supertest');
 let should = require('should');
 
 describe('test for router', function() {
   describe('GET', function() {
-    it('get props', function* () {
-      yield request(app)
-        .get('/posts/id')
+    it('get props', function (done) {
+      request(app.listen())
+        .get('/posts/0/id')
+        .end(function (err, res) {
+          res.text.should.be.eql('1');
+          done();
+        });
+    });
+
+    it('get props with query', function (done) {
+      request(app.listen())
+        .get('/posts/0?id=1')
         .expect(200)
-        .end();
-    })
+        .end(function (err, res) {
+          res.body.should.containEql({id: '1'});
+          done();
+        });
+    });
   });
 });
