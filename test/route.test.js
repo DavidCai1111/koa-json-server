2543b50d89ff03dc16583233470af699faefc026
DavidCai <376462191@qq.com>
2015-08-29 13:42:05 +0800

    put

--- test/route.test.js
+++ test/route.test.js
@@ -48,4 +48,37 @@ describe('test for router', function() {
     });
   });
 
+  describe('PUT', function() {
+    it('change props', function (done) {
+      request(app.listen())
+        .put('/posts/0')
+        .send({'id': '2'})
+        .end(function (err, res) {
+          res.body.should.containEql({id: '2'});
+          done();
+        })
+    });
+
+    it('change undefined props', function (done) {
+      request(app.listen())
+        .put('/posts/0')
+        .send({'idd': '2'})
+        .end(function (err, res) {
+          res.body.should.not.containEql({idd: '2'});
+          done();
+        })
+    });
+
+    //tmp
+    it('change props back', function (done) {
+      request(app.listen())
+        .put('/posts/0')
+        .send({'id': '1'})
+        .end(function (err, res) {
+          res.body.should.containEql({id: '1'});
+          done();
+        })
+    });
+  });
+
 });
